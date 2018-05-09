75d5a70e1a0356159d5ef3b404231e82fd5c3baf
jenny <839106161@qq.com>
2018-05-09 14:38:00 +0800

    城市搜索

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -1,12 +1,17 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ travel/src/pages/city/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+      <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl';
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input
+      box-sizing border-box
+      width 100%
+      height .62rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      padding 0 .1rem
+</style>
