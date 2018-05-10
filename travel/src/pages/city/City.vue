5f30ad6a0a4b4af7ffd81007bebc02bf0a26bf3a
jenny <839106161@qq.com>
2018-05-10 14:14:59 +0800

    city-list

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -2,11 +2,12 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
+    <city-list :hotCities="hotCities" :cities="cities"></city-list>
     <city-alphabet></city-alphabet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
@@ -18,6 +19,27 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('api/city.json').then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      console.log(res)
+      if (res.data.ret && res.data) {
+        this.cities = res.data.data.cities
+        this.hotCities = res.data.data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
